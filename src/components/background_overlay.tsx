8371a839de3cddf9e5bd766698f5faf5423e4ca1
Theerapong Bank <theerapong.bvnk@gmail.com>
2024-04-24 01:48:24 +0700

    Update background_overlay.tsx

--- src/components/background_overlay.tsx
+++ src/components/background_overlay.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
-import bg from "@/assets/svg/bg.svg";
+import bg from "../assets/svg/bg.svg";
 
 export default function BackgroundOverlay() {
   return (
