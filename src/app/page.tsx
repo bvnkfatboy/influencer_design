8cdbc816079751e20588af5969300c51fb818652
Theerapong Bank <theerapong.bvnk@gmail.com>
2024-04-24 15:44:13 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <main>
       <Background />
       <div className="flex flex-col justify-center items-center sm:gap-x-12">
-        <div className="relative mt-10 w-full max-w-[384px] max-[425px]:max-w-[350px]  sm:w-auto min-[1024px]:absolute min-[1024px]:left-[50%] min-[1024px]:top-[15%]">
+        <div className="relative mt-10 w-full max-w-[384px] max-[425px]:max-w-[350px]  sm:w-auto min-[1024px]:absolute min-[1024px]:left-[50%] min-[1024px]:top-[7%]">
           <div className="text-gray-600 search-infu">
             <form className="rounded-full p-2 bg-gradient-to-r from-[#8359b9] to-[#263ea0]">
               <input
@@ -39,7 +39,7 @@ export default function Home() {
             </form>
           </div>
         </div>
-        <div className="section-1  min-[1024px]:mt-[5.5rem] min-[768px]:mt-[2rem] max-[425px]:mt-[2rem]">
+        <div className="section-1  min-[1024px]:mt-[0rem] min-[768px]:mt-[2rem] max-[425px]:mt-[2rem]">
           <DrawImage
             src={Frame_1}
             width={1091}
@@ -52,7 +52,7 @@ export default function Home() {
           <Link
             href="#"
             size="sm"
-            className="relative md:left-[10rem] lg:left-[13.5rem] xl:left-[15rem] 2xl:left-[14.5rem]"
+            className="relative md:left-[10rem] lg:left-[13.5rem] xl:left-[15rem] 2xl:left-[14.5rem] md:-mt-5"
           >
             <DrawImage
               src={SignUpIcon}
